3c19e750fe6ddbe4697046834b0dbed25a6df0a3
Skyblueluke <145379868+Skyblueluke@users.noreply.github.com>
2026-03-02 15:13:46 +0000

    Update Snake Game.py

--- Snake Game.py
+++ Snake Game.py
@@ -1,128 +1,214 @@
-import pygame
+import pygame # Imports pygame and pygame library
 
-pygame.init()
+import random # Imports random for the apples
+
+pygame.init() # Activates pygame
 
 # Flags
-running = True
-menu_run = True
-difficulty_run = False
-game_run = False
-selected_difficulty = "Medium"
-snake_speed = 13
+running = True # This is the whole game loop (always active)
+menu_run = True # This is the menu loop (currently active)
+difficulty_run = False # This is the difficuty loop (currently inactive)
+game_run = False # This is the playing lood (currently inactive)
+selected_difficulty = "Medium" # This is the game difficulty variable initially set at medium
+snake_speed = 13 # This is the snake speed which is a linked variable to the difficulty
+
+# Snake settings
+BLOCK_SIZE = 20 # This is the size of a single grid square in pixels
+snake = [(400, 400), (380, 400), (360, 400)] # The snake is a list of tuples, 1st tuple is the head and the other 2 are the body
+direction = "RIGHT" # This is the current direction of the snake
+change_to = direction # This stores the next direction
 
 # Rects
-start_button = pygame.Rect(346, 264, 112, 32)
-difficulty_button = pygame.Rect(278, 465, 263, 30)
-quit_button = pygame.Rect(345, 664, 109, 32)
+play_button = pygame.Rect(346, 264, 112, 32) # These are the play  button co ordinates
+difficulty_button = pygame.Rect(278, 465, 263, 30) # These are the difficulty button co ordinates
+quit_button = pygame.Rect(345, 664, 109, 32) # There are the quid button co ordinates
 
-easy_button = pygame.Rect(346, 254, 464-346, 285-254)
-medium_button = pygame.Rect(317, 374, 496-317, 404-374)
-hard_button = pygame.Rect(347, 495, 471-347, 526-495)
-back_button = pygame.Rect(348, 614, 469-348, 644-614)
+easy_button = pygame.Rect(346, 254, 464-346, 285-254) # These are the easy  button co ordinates
+medium_button = pygame.Rect(317, 374, 496-317, 404-374) # These are the medium  button co ordinates
+hard_button = pygame.Rect(347, 495, 471-347, 526-495) # These are the play  hard co ordinates
+back_button = pygame.Rect(348, 614, 469-348, 644-614) # These are the play  back co ordinates
 
-# Screen and colors
-WIDTH, HEIGHT = 800, 800
-green = (14,209,28)
-black = (0,0,0)
-SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Snake Game")
+# Screen and colours
+WIDTH, HEIGHT = 800, 800 # This is the window size
+green = (14,209,28) # This is green being established as a colour
+black = (0,0,0) # This is black being established as a colour
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT)) # This is the screen being made
+pygame.display.set_caption("Snake Game") # This is the nig Snake Game message at the top of the screen
 
 # Fonts
-header = pygame.font.SysFont("arialblack", 80)
-font = pygame.font.SysFont("arialblack", 40)
-text_colour = green
+header = pygame.font.SysFont("arialblack", 80) # This is the header font being established
+font = pygame.font.SysFont("arialblack", 40) # This is the standard font being established
+text_colour = green  # This is the font colour being established
 
-def write_text(text, font, text_colour, x, y):
-    img = font.render(text, True, text_colour)
-    SCREEN.blit(img, (x,y))
+def write_text(text, font, text_colour, x, y): # These are the the necessary parts of the funtion designed to make a new word on the screen
+    img = font.render(text, True, text_colour) # This writes the message
+    SCREEN.blit(img, (x,y)) # This puts it on the screen
 
-#Grid
-BLOCK_SIZE = 20
-snake = [(400, 400), (380, 400), (360, 400)]
-direction = "RIGHT"
+# Grid
+BLOCK_SIZE = 20 # This is the size of a single grid square in pixels
+snake = [(400, 400), (380, 400), (360, 400)] # The snake is a list of tuples, 1st tuple is the head and the other 2 are the body
+direction = "RIGHT" # This is the current direction of the snake
 
+# Apple making function
+def spawn_apple(): # This makes the function 'spawn_apple'
+    while True: # While spawn_apple is true
+        x = random.randrange(0, (WIDTH - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE # Anywhere widthwise in the window
+        y = random.randrange(0, (HEIGHT - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE # Anywhere depthwise in the window
+        if (x, y) not in snake: # If it is not on the snake
+            return (x, y) # Place it
+        
 # Main loop
-while running:
+while running: # This is the beginning of the actual loop - running is always True
     SCREEN.fill(black)  # Clear screen at start of frame
 
     # Event handling
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    for event in pygame.event.get(): # This is the beginning of the loop that says if x happens do y
+        if event.type == pygame.QUIT: # This is for the quit button, it will end the program, and this ends the running loop
+            running = False # This is the end of the running loop
 
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if menu_run:
-                if start_button.collidepoint(event.pos):
-                    print("Play pressed")
-                    menu_run = False
-                    game_run = True
-                elif difficulty_button.collidepoint(event.pos):
-                    print("Difficulty pressed")
-                    menu_run = False
-                    difficulty_run = True
-                    game_run = False
-                elif quit_button.collidepoint(event.pos):
-                    running = False
-            elif difficulty_run:
-                if easy_button.collidepoint(event.pos):
-                    selected_difficulty = "Easy"
-                    difficulty_run = False
-                    menu_run = True
-                    snake_speed = 8
-                elif medium_button.collidepoint(event.pos):
-                    selected_difficulty = "Medium"
-                    difficulty_run = False
-                    menu_run = True
-                    snake_speed = 13
-                elif hard_button.collidepoint(event.pos):
-                    selected_difficulty = "Hard"
-                    difficulty_run = False
-                    menu_run = True
-                    snake_speed = 18
-                elif back_button.collidepoint(event.pos):
-                    difficulty_run = False
-                    menu_run = True
+        # Moving the snake
+        if event.type == pygame.KEYDOWN and game_run: # If a key is pressed loop
+            if event.key == pygame.K_w and direction != "DOWN": # If w is pressed and not going in opposite direction
+                change_to = "UP" # Change next snake direction to up
+            elif event.key == pygame.K_s and direction != "UP": # If s is pressed and not going in opposite direction
+                change_to = "DOWN" # Change next snake direction to up
+            elif event.key == pygame.K_a and direction != "RIGHT": # If a is pressed and not going in opposite direction
+                change_to = "LEFT" # Change next snake direction to up
+            elif event.key == pygame.K_d and direction != "LEFT": # If d is pressed and not going in opposite direction
+                change_to = "RIGHT" # Change next snake direction to up
+
+        elif event.type == pygame.MOUSEBUTTONDOWN: # If the mouse is clicked loop
+            if menu_run: # This is the menu loop, it is also the loop that origninally starts due to it being True initially in the flags section
+                if play_button.collidepoint(event.pos): # If the mouse is clicked on the play button (established in the rects section)
+                    print("Play pressed") # This was for checking if the button was recognised as pressed before there were menu changes
+                    menu_run = False # Menu loop ends
+                    game_run = True # Playing loop starts
+                    apple_pos = spawn_apple() # This is the start of the apple spawning process
+                elif difficulty_button.collidepoint(event.pos): # If the mouse is clicked on the difficulty button (established in the rects section)
+                    print("Difficulty pressed") # This was for checking if the button was recognised as pressed before there were menu changes
+                    menu_run = False # Menu loop ends
+                    difficulty_run = True # Difficulty menu loop starts
+                    game_run = False # Playing loop ends
+                elif quit_button.collidepoint(event.pos): # If the mouse is clicked on the play button (established in the rects section)
+                    running = False # Whole game loop ends
+            elif difficulty_run: # If the difficulty menu loop is active
+                if easy_button.collidepoint(event.pos): #  # If the mouse is clicked on the easy button (established in the rects section)
+                    selected_difficulty = "Easy" # The difficulty is set to easy
+                    difficulty_run = False # Difficulty menu loop ends
+                    menu_run = True # Menu loop starts again
+                    snake_speed = 8 # This is the snakes speed in the easy difficulty 
+                elif medium_button.collidepoint(event.pos): # If the mouse is clicked on the medium button (established in the rects section)
+                    selected_difficulty = "Medium" # The difficulty is set to medium
+                    difficulty_run = False # Difficulty menu loop ends
+                    menu_run = True # Menu loop starts again
+                    snake_speed = 13 # This is the snakes speed in the medium difficulty 
+                elif hard_button.collidepoint(event.pos): # If the mouse is clicked on the hard button (established in the rects section)
+                    selected_difficulty = "Hard"  # The difficulty is set to hard
+                    difficulty_run = False # Difficulty menu loop ends
+                    menu_run = True # Menu loop starts again
+                    snake_speed = 18 # This is the snakes speed in the hard difficulty 
+                elif back_button.collidepoint(event.pos): # If the mouse is clicked on the back button (established in the rects section)
+                    difficulty_run = False  # Difficulty menu loop ends
+                    menu_run = True # Menu loop starts again
+
+               
+
+                for event in pygame.event.get(): # This is the beginning of the loop that says if x happens do y
+                    if event.type == pygame.KEYDOWN: # If a key is pressed loop
+                        if event.key == pygame.K_ESCAPE and game_run: # If k is pressed and the play loop is active
+                            game_run = False # Playing menu loop ends
+                            menu_run = True # Menu loop starts again
 
     # Draw the appropriate menu
-    if menu_run:
-        write_text("SNAKE", header, text_colour, 250, 50)
-        write_text("PLAY", font, text_colour, 345, 250)
-        write_text("DIFFICULTY", font, text_colour, 275, 450)
-        write_text("QUIT", font, text_colour, 345, 650)
+    if menu_run: # If the menu loop is running
+        write_text("SNAKE", header, text_colour, 250, 50) #Write SNAKE in the header font (established in the fonts section)
+        write_text("PLAY", font, text_colour, 345, 250) #Write PLAY in the standard font (established in the fonts section)
+        write_text("DIFFICULTY", font, text_colour, 275, 450) #Write DIFFICULTY in the standard font (established in the fonts section)
+        write_text("QUIT", font, text_colour, 345, 650) #Write QUIT in the standard font (established in the fonts section)
 
-        pygame.draw.rect(SCREEN, (255, 0, 0), start_button, 1)
-        pygame.draw.rect(SCREEN, (0, 255, 0), difficulty_button, 1)  
-        pygame.draw.rect(SCREEN, (0, 0, 255), quit_button, 1)
+        pygame.draw.rect(SCREEN, (255, 0, 0), play_button, 1) # Makes a hitbox in red around the play button for debugging purposes
+        pygame.draw.rect(SCREEN, (0, 255, 0), difficulty_button, 1) # Makes a hitbox in red around the difficulty button for debugging purposes
+        pygame.draw.rect(SCREEN, (0, 0, 255), quit_button, 1) # Makes a hitbox in red around the quit button for debugging purposes
 
-    elif difficulty_run:
-        write_text("DIFFICULTY", header, text_colour, 150, 50)
-        write_text("EASY", font, text_colour, 345, 240)
-        write_text("MEDIUM", font, text_colour, 315, 360)
-        write_text("HARD", font, text_colour, 345, 480)
-        write_text("BACK", font, text_colour, 345, 600)
+    elif difficulty_run: # If the difficulty loop is running
+        write_text("DIFFICULTY", header, text_colour, 150, 50) #Write DIFFICULTY in the header font (established in the fonts section)
+        write_text("EASY", font, text_colour, 345, 240) #Write EASY in the standard font (established in the fonts section
+        write_text("MEDIUM", font, text_colour, 315, 360) #Write MEDIUM in the standard font (established in the fonts section
+        write_text("HARD", font, text_colour, 345, 480) #Write HARD in the standard font (established in the fonts section
+        write_text("BACK", font, text_colour, 345, 600) #Write BACK in the standard font (established in the fonts section
 
-        pygame.draw.rect(SCREEN, (255,0,0), easy_button, 1)
-        pygame.draw.rect(SCREEN, (255,255,0), medium_button, 1)
-        pygame.draw.rect(SCREEN, (0,0,255), hard_button, 1)
-        pygame.draw.rect(SCREEN, (0,255,255), back_button, 1)
+        pygame.draw.rect(SCREEN, (255,0,0), easy_button, 1) # Makes a hitbox in red around the easy button for debugging purposes
+        pygame.draw.rect(SCREEN, (255,255,0), medium_button, 1) # Makes a hitbox in red around the medium button for debugging purposes
+        pygame.draw.rect(SCREEN, (0,0,255), hard_button, 1) # Makes a hitbox in red around the hard button for debugging purposes
+        pygame.draw.rect(SCREEN, (0,255,255), back_button, 1) # Makes a hitbox in red around the back button for debugging purposes
 
-    elif game_run:
-        SCREEN.fill(black)
+    elif game_run: # If the playing loop is running
+        SCREEN.fill(black) # Make the whole window black
 
-        for segment in snake:
-            pygame.draw.rect(
-                SCREEN,
-                green,
-                pygame.Rect(segment[0], segment[1], BLOCK_SIZE, BLOCK_SIZE)
-            )
+        # Making the snake
+        for segment in snake: # This established the snake as a list of segments (These segments will becom ea tuple)
+            pygame.draw.rect(SCREEN, green, pygame.Rect(segment[0], segment[1], BLOCK_SIZE, BLOCK_SIZE)) # Draws a green recangle on the screen with a tuple of segments
 
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE and game_run:
-                    game_run = False
-                    menu_run = True
+            #Stopping illegal moves
+            if change_to == "UP" and direction != "DOWN": # If the direction is up and next direction is not down
+                direction = "UP" # Make new direction up
+            elif change_to == "DOWN" and direction != "UP": # If the direction is down and next direction is not up
+                direction = "DOWN" # Make new direction down
+            elif change_to == "LEFT" and direction != "RIGHT": # If the direction is left and next direction is not right
+                direction = "LEFT" # Make new direction left
+            elif change_to == "RIGHT" and direction != "LEFT": # If the direction is right and next direction is not left
+                direction = "RIGHT" # Make new direction right
+                #This means the snake will only move legally
 
+            # Making the apple
+            pygame.draw.rect(SCREEN,(255, 0, 0),pygame.Rect(apple_pos[0], apple_pos[1], BLOCK_SIZE, BLOCK_SIZE))# Draws a red square on the screen as a single segment
+
+      
+
+
+            # Move snake head
+            x, y = snake[0] # This splits the snake's head into an x and y co ordinate
+
+            if direction == "UP": # If the current direction is up
+                y -= BLOCK_SIZE # Y co ordinate decreases
+            elif direction == "DOWN": # If the current direction is up
+                y += BLOCK_SIZE # Y co ordinate increases
+            elif direction == "LEFT": # If the current direction is up
+                x -= BLOCK_SIZE # X co ordinate decreases
+            elif direction == "RIGHT": # If the current direction is up
+                x += BLOCK_SIZE # X co ordinate increases
+
+            snake.insert(0, (x, y)) # This adds a new head in front of the last head
+            snake.pop() # This removes the final part of the tail, giving the illusion that the snake moves
+
+
+            x, y = snake[0]
+
+            if direction == "UP": # If the current direction is up
+                y -= BLOCK_SIZE # Y co ordinate decreases
+            elif direction == "DOWN": # If the current direction is down
+                y += BLOCK_SIZE # Y co ordinate increases
+            elif direction == "LEFT": # If the current direction is left
+                x -= BLOCK_SIZE # X co ordinate decreases
+            elif direction == "RIGHT": # If the current direction is right
+                x += BLOCK_SIZE # X co ordinate increases
+
+            new_head = (x, y) # This makes the new head
+            snake.insert(0, new_head) # This visually adds it
+
+            # Apple collision
+            if new_head == apple_pos: # If the apple and snake collide
+                apple_pos = spawn_apple()  # The snake grows
+            else: # If the apple and the snake do not collide
+                snake.pop() # This removes the final part of the tail, giving the illusion that the snake moves
+
+            for segment in snake: # For each part of the snake
+                pygame.draw.rect(SCREEN, green, pygame.Rect(segment[0], segment[1], BLOCK_SIZE, BLOCK_SIZE)) # This draws a green tuple-based rectangle
+
+            clock = pygame.time.Clock() # This makes a clock that the game will run by
+
+            clock.tick(snake_speed) # The clock is directly linked the the difficulty and snake speed
                         
-    pygame.display.update()
+    pygame.display.update() # This commands the screen to update at all times in the main loop
 
-pygame.quit()
+pygame.quit() # Closes the program
